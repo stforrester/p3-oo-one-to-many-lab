4bea58c411d6a0aef54542f9177165b67b026415
Stuart Forrester <stforrester@gmail.com>
2025-01-03 06:10:01 -0600

    completes lab and passes all tests

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,41 @@
 class Pet:
-    pass
+    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+
+    def __init__(self, name, pet_type, owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        if isinstance(owner, Owner):
+            self.owner = owner
+        Pet.all.append(self)
+
+    @property
+    def pet_type(self):
+        return self._pet_type
+    
+    @pet_type.setter
+    def pet_type(self, value):
+        if value not in Pet.PET_TYPES:
+            raise TypeError(f"{value} is not a valid pet type.")
+        else:
+            self._pet_type = value
 
 class Owner:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+    
+    def pets(self):
+        return list(pet for pet in Pet.all if pet.owner == self)
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("pet must be an instance of the Pet Class")
+        else:
+            pet.owner = self
+
+    def get_sorted_pets(self):
+        unsorted_pets = self.pets()
+        return sorted(unsorted_pets, key=lambda pet: pet.name)
+
